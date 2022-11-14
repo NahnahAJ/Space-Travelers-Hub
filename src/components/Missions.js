c3eba5bd6ff2d169ffc4580ef00779b62d5ff2bf
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-14 20:50:39 +0000

    Display missions received from the API in a table

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Table } from 'react-bootstrap';
+import { fetchMissions } from '../redux/missions/missions';
 
-export default function Missions() {
+const Missions = () => {
+  const newMissions = useSelector((state) => state.mission);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (fetchMissions.isLoading === false) {
+      dispatch(fetchMissions());
+    }
+  }, [fetchMissions.isLoading, dispatch]);
+
+  const receivedMissions = newMissions.missions;
+  console.log(receivedMissions);
   return (
-    <h1>Missions</h1>
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          {receivedMissions.map((mission) => (
+            <tr key={mission.mission_id}>
+              <td>{mission.mission_name}</td>
+              <td>{mission.description}</td>
+              <td>
+                <button type="button">Not a member</button>
+              </td>
+              <td>
+                <button type="button">Join Mission</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
   );
-}
+};
+
+export default Missions;
