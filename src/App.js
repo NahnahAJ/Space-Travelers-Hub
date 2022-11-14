4d524b61ad0e8a3e01cf397b27afbf2cb5b178d9
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-14 11:43:27 +0000

    Install linters and fix errors

--- src/App.js
+++ src/App.js
@@ -4,22 +4,20 @@ import Navbar from './components/Navbar';
 import logo from './images/planet.png';
 import MyProfile from './components/MyProfile';
 
-const App = () => {
-  return (
-    <>
-      <header>
-        <div>
-          <img src={logo} alt="logo" />
-          <h1>Space Travelers' Hub</h1>
-        </div>
-        <Navbar />
-      </header>
+const App = () => (
+  <>
+    <header>
+      <div>
+        <img src={logo} alt="logo" />
+        <h1>Space Travelers&apos; Hub</h1>
+      </div>
+      <Navbar />
+    </header>
 
-      <Routes>
-        <Route path="/profile" element={<MyProfile />}></Route>
-      </Routes>
-    </>
-  );
-};
+    <Routes>
+      <Route path="/profile" element={<MyProfile />} />
+    </Routes>
+  </>
+);
 
 export default App;
