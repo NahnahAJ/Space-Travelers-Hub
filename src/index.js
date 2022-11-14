d3d08875b033e17d16fc3bca680bd81e9f49e454
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-14 20:51:46 +0000

    Initialize redux store and import bootsrap

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
-// import { Provider } from'react-redux';
+import store from './redux/configureStore';
+import { fetchMissions } from './redux/missions/missions';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+store.dispatch(fetchMissions());
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
