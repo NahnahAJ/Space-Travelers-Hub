2c7fefc30991b2fd4300a194da72d2f82e09b1db
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-15 09:22:36 +0000

    Fix import conflict

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configureStore';
 import { fetchMissions } from './redux/missions/missions';
