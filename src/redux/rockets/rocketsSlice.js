abbafaaa5a2770dc3e354589f760fe073f640bd1
amon-cofie <ddd.amon123@gmail.com>
2022-11-15 08:39:02 +0000

    Update action to get data from the rockets data endpoint

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -2,23 +2,21 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const URL = 'https://api.spacexdata.com/v3/rockets';
+const GET_ROCKETS = 'rockets/getRockets';
 
 const initialState = {
   rockets: [],
   isLoading: true,
 };
 
-export const asyncGetRockets = createAsyncThunk(
-  'rockets/getRockets',
-  async () => {
-    try {
-      const response = await axios.get(URL);
-      return response.data;
-    } catch (error) {
-      return error;
-    }
-  },
-);
+export const asyncGetRockets = createAsyncThunk(GET_ROCKETS, async () => {
+  try {
+    const response = await axios.get(URL);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+});
 
 const rocketsSlice = createSlice({
   name: 'rocketsReducer',
@@ -26,8 +24,6 @@ const rocketsSlice = createSlice({
   reducers: [],
   extraReducers: {
     [asyncGetRockets.fulfilled]: (state, action) => {
-      // console.log(action.payload);
-      // console.log('working');
       state.rockets = action.payload;
       state.isLoading = false;
     },
