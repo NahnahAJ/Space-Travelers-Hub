1fa196c2ff11308a8052430982b4bf2c3549bc35
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-16 09:45:55 +0000

    Dispatch actions for join and leave mission and switch badges for conditional components

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Table, Button } from 'react-bootstrap';
-import { fetchMissions } from '../redux/missions/missions';
+import { fetchMissions, joinMissions, leaveMission } from '../redux/missions/missions';
 
 const Missions = () => {
   const newMissions = useSelector((state) => state.mission);
   const dispatch = useDispatch();
+
+  const joinHandler = (id) => {
+    dispatch(joinMissions(id));
+  };
+
+  const leaveMissionHandler = (id) => {
+    dispatch(leaveMission(id));
+  };
 
   useEffect(() => {
     if (fetchMissions.isLoading === false) {
@@ -26,14 +34,14 @@ const Missions = () => {
         </thead>
         <tbody>
           {receivedMissions.map((mission) => (
-            <tr key={mission.mission_id}>
-              <td>{mission.mission_name}</td>
+            <tr key={mission.missionId}>
+              <td>{mission.missionName}</td>
               <td>{mission.description}</td>
               <td>
-                <Button variant="secondary" type="button">Not a member</Button>
+                <span>{mission.reserved ? <Button type="button">Active Member</Button> : <Button type="button" variant="secondary">Not a member</Button>}</span>
               </td>
               <td>
-                <Button variant="outline-dark" type="button">Join Mission</Button>
+                <span>{mission.reserved ? <Button type="button" variant="outline-danger" onClick={() => { leaveMissionHandler(mission.missionId); }}>Leave Reservation</Button> : <Button variant="outline-dark" type="submit" onClick={() => joinHandler(mission.missionId)}>Join Mission</Button>}</span>
               </td>
             </tr>
           ))}
