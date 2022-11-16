5073a0c91d5643d5de5baa89e1c54e888fb3be89
amon-cofie <ddd.amon123@gmail.com>
2022-11-16 12:07:21 +0000

    Display booked rockets on profile page

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Table } from 'react-bootstrap';
 
 export default function MyProfile() {
+  const rockets = useSelector((state) => state.rockets.rockets);
+
   return (
-    <h1>MyProfile</h1>
+    <>
+      <h4>My Rockets</h4>
+      <Table bordered hover>
+        <tbody>
+          {rockets
+            .filter((rocket) => rocket.reserved === true)
+            .map((filteredRocket) => (
+              <tr key={filteredRocket.id}>
+                <td>{filteredRocket.rocketName}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </>
   );
 }
