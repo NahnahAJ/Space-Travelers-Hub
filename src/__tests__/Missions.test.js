cd56fdebb3e419de62f0246c1fbd433fa6ccb196
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-17 16:36:15 +0000

    Fix linter errors

--- src/__tests__/Missions.test.js
+++ src/__tests__/Missions.test.js
@@ -1,51 +1,51 @@
-import Missions from '../components/Missions';
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import store from '../redux/configureStore'
+import store from '../redux/configureStore';
+import Missions from '../components/Missions';
 
 const renderComponent = () => {
   render(
     <Provider store={store}>
       <Missions />
     </Provider>,
-  )
+  );
 };
 
 describe('Testing Missions Table component', () => {
   test('Check mission title', () => {
     renderComponent();
-    let missionHeader = screen.getByText(/Mission/i);
+    const missionHeader = screen.getByText(/Mission/i);
     expect(missionHeader).toBeInTheDocument();
   });
 
   test('Check description title', () => {
     renderComponent();
-    let descriptionHeader = screen.getByText(/Description/i);
+    const descriptionHeader = screen.getByText(/Description/i);
     expect(descriptionHeader).toBeInTheDocument();
   });
 
   test('Check status in DOM', () => {
     renderComponent();
-    let statusHeader = screen.getByText(/Status/i);
+    const statusHeader = screen.getByText(/Status/i);
     expect(statusHeader).toBeInTheDocument();
-  })
+  });
 
-  test('Check if the Table is in the dom', () => { 
+  test('Check if the Table is in the dom', () => {
     renderComponent();
-    let tableElement = screen.getByRole('table');
+    const tableElement = screen.getByRole('table');
     expect(tableElement).toBeInTheDocument();
-  })
+  });
 
-  test('Check if there are a group of rows', () => { 
+  test('Check if there are a group of rows', () => {
     renderComponent();
-    let rowsGroup = screen.getAllByRole('rowgroup');
+    const rowsGroup = screen.getAllByRole('rowgroup');
     expect(rowsGroup.length).toBeGreaterThan(1);
-  })
+  });
 
-  test('Check if the row element is in the dom', () => { 
+  test('Check if the row element is in the dom', () => {
     renderComponent();
-    let rowElement = screen.getByRole('row');
+    const rowElement = screen.getByRole('row');
     expect(rowElement).toBeInTheDocument();
-  })
+  });
 });
